c7eb6fccf065b4651276d23909dec9efb3e02c12
David Luce <david.luce@outlook.com>
2019-06-18 23:05:50 -0600

    add aJax call and function to search reasults in API.

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,9 @@
-$("#search").on("click", function () {
+$("#run-search").on("click", function () {
+    var gifSearch = $('#search-term').val()
+console.log(gifSearch)
 
 
-
-
-
-    var queryURL = $.get("https://api.giphy.com/v1/gifs/search?api_key=EZPRe6jQskg48yfyY2aqEb1gGO76vosj&q=string&limit=25&offset=0&rating=G&lang=en");
+    var queryURL = (`https://api.giphy.com/v1/gifs/search?api_key=EZPRe6jQskg48yfyY2aqEb1gGO76vosj&q=${gifSearch}&limit=25&offset=0&rating=G&lang=en`);
 
     $.ajax({
         url: queryURL,
