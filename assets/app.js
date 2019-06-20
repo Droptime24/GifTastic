1ca6107cbc40140600c352df3fb64b54744351e0
David Luce <david.luce@outlook.com>
2019-06-19 21:54:10 -0600

    updated js

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,47 @@
-$("#run-search").on("click", function () {
-    var gifSearch = $('#search-term').val()
-console.log(gifSearch)
+$(document).ready(function () {
+    // Buttons to render
+    var topics = ["video games", "books", "music", "computer code"];
 
 
-    var queryURL = (`https://api.giphy.com/v1/gifs/search?api_key=EZPRe6jQskg48yfyY2aqEb1gGO76vosj&q=${gifSearch}&limit=25&offset=0&rating=G&lang=en`);
+    function renderButtons() {
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
+        $("#buttons").empty();
 
+        for (var i = 0; i < topics.length; i++) {
+            var bttn = $("<button>" + topics[i] + "</button>");
+            bttn.addClass("games");
+            bttn.attr("data-name", topics[i]);
+            bttn.text(topics[i]);
+            $('#buttons').append(bttn);
 
+        }
+
+    };
+    renderButtons()
+    // search for gifs
+    $("#buttons").on("click", ".games", function () {
+
+        // var gifSearch = $('#search').val()
+       var gifSearch = $(this).attr("data-name");
+
+        var queryURL = (`https://api.giphy.com/v1/gifs/search?api_key=EZPRe6jQskg48yfyY2aqEb1gGO76vosj&q=${gifSearch}&limit=10&offset=0&rating=G&lang=en`);
+
+        console.log(gifSearch)
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response.data;
+            console.log(results)
+            for (var i = 0; i < results.length; i++) {
+                console.log(results)
+                var gifs = $('<img>');
+                gifs.attr("src", results[i].images.fixed_height.url);
+                $("#gifs").append(gifs);
+            }
+        })
 
     });
-
 });
