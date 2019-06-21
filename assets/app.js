f56fc0f8e026b067da846ef524aad70bc9ff8920
David Luce <david.luce@outlook.com>
2019-06-20 23:15:19 -0600

    updated html, js, trying to create new buttons in array

--- assets/app.js
+++ assets/app.js
@@ -1,47 +1,67 @@
-$(document).ready(function () {
-    // Buttons to render
-    var topics = ["video games", "books", "music", "computer code"];
+
+// Buttons to render
+var topics = ["video games", "books", "music", "computer code"];
 
 
-    function renderButtons() {
+// search for gifs
+function displayGifs() {
 
-        $("#buttons").empty();
+    // var gifSearch = $('#search').val()
+    var gifSearch = $(this).attr("data-name");
 
-        for (var i = 0; i < topics.length; i++) {
-            var bttn = $("<button>" + topics[i] + "</button>");
-            bttn.addClass("games");
-            bttn.attr("data-name", topics[i]);
-            bttn.text(topics[i]);
-            $('#buttons').append(bttn);
+    var queryURL = (`https://api.giphy.com/v1/gifs/search?api_key=EZPRe6jQskg48yfyY2aqEb1gGO76vosj&q=${gifSearch}&limit=10&offset=0&rating=G&lang=en`);
 
+    console.log(gifSearch);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        console.log(queryURL)
+        var results = response.data;
+        console.log(results)
+
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i]);
+            var gifs = $('<img>');
+            gifs.attr("src", results[i].images.fixed_height.url);
+            $("#gifs").append(gifs);
         }
-
-    };
-    renderButtons()
-    // search for gifs
-    $("#buttons").on("click", ".games", function () {
-
-        // var gifSearch = $('#search').val()
-       var gifSearch = $(this).attr("data-name");
-
-        var queryURL = (`https://api.giphy.com/v1/gifs/search?api_key=EZPRe6jQskg48yfyY2aqEb1gGO76vosj&q=${gifSearch}&limit=10&offset=0&rating=G&lang=en`);
-
-        console.log(gifSearch)
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            var results = response.data;
-            console.log(results)
-            for (var i = 0; i < results.length; i++) {
-                console.log(results)
-                var gifs = $('<img>');
-                gifs.attr("src", results[i].images.fixed_height.url);
-                $("#gifs").append(gifs);
-            }
-        })
-
     });
+}
+        
+       
+// render buttons and display
+function renderButtons() {
+
+    // clear the div
+    $("#buttons").empty();
+
+    // loop array
+    for (var i = 0; i < topics.length; i++) {
+        var b = $(`<button class="btn btn-primary"</button>`);
+        b.addClass("gifs");
+        b.attr("data-name", topics[i]);
+        b.text(topics[i]);
+        $('#buttons').prepend(b);
+
+    }
+
+}
+// This function handles events where a movie button is clicked
+$("#add-gifs").on("click", ".games", function (event) {
+    event.preventDefault();
+    var gifAdd = $("#add-gifs").val().trim();
+    add.push(gifAdd);
+    console.log(createGif)
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
 });
+
+// Adding a click event listener to all elements with a class of "movie-btn"
+$(document).on("click", ".gifs", displayGifs);
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
+
